9ba37da502878c97c72eb779e74446bd036f9e03
363531807 <363531807@qq.com>
2015-09-17 10:08:36 +0800

    initmore

--- teacherclient/src/main/java/com/a363531807/teacherclient/MainActivity.java
+++ teacherclient/src/main/java/com/a363531807/teacherclient/MainActivity.java
@@ -1,4 +1,4 @@
-package com.a363531807.curriculumdesign;
+package com.a363531807.teacherclient;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
