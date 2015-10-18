98fb840617012efcdfa196d31f95efd735ea8156
363531807 <363531807@qq.com>
2015-10-18 16:15:21 +0800

    initmore

--- teacherclient/src/main/java/com/a363531807/teacherclient/MyRecordActivity.java
+++ teacherclient/src/main/java/com/a363531807/teacherclient/MyRecordActivity.java
@@ -16,11 +16,13 @@ public class MyRecordActivity extends AppCompatActivity {
         Map<String,String> map = (Map<String, String>) getIntent().getExtras().getSerializable("result");
         TextView name =(TextView)findViewById(R.id.tv_my_name);
         TextView sign =(TextView)findViewById(R.id.tv_my_sign);
+        TextView title =(TextView)findViewById(R.id.tv_my_title);
         TextView total =(TextView)findViewById(R.id.tv_course_total_num);
         TextView untotal =(TextView)findViewById(R.id.tv_course_untotal_num);
         RatingBar rate =(RatingBar)findViewById(R.id.rb_course_total_rate);
         name.setText(map != null ? map.get("name") : null);
         sign.setText(map != null ? map.get("sign") : null);
+        title.setText(map != null ? map.get("title") : null);
         total.setText(map != null ? map.get("total") : null);
         untotal.setText(map != null ? map.get("untotal") : null);
         rate.setRating(map != null ? Float.parseFloat(map.get("rate")) : 5.0f);
